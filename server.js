41b680997084e719cebc5a107c301b8b9b22c1cc
shreyapattewar <shreya.pattewar1999@gmail.com>
2023-07-02 21:50:30 +0530

    cors error res header updated

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(
 );
 var allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Accept-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
